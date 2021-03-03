2e0b7a3be004e19bca6bf07fcdcb737201caa054
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2021-03-03 11:30:36 -0300

    feature: Add calculator with fuctions: sum, subtraction, division and multiplication

---
+++ src/calculator.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+  return a + b;
+}
+
+const subtraction = (a, b) => {
+  return a - b;
+}
+
+const division = (a, b) => {
+  return a / b;
+}
+
+const multiplication = (a, b) => {
+  return a * b;
+}
+
+module.exports = {
+  sum,
+  subtraction,
+  division,
+  multiplication,
+}
---
+++ src/calculator.test.js
@@ -0,0 +1,25 @@
+const calculator = require('./calculator');
+
+test('Sum 10 + 10 to be equal to 20', () => {
+  const expectedValue = 20;
+  const result = calculator.sum(10, 10);
+  expect(result).toBe(expectedValue);
+})
+
+test('Subtract 30 - 10 to be equal to 20', () => {
+  const expectedValue = 20;
+  const result = calculator.subtraction(30, 10);
+  expect(result).toBe(expectedValue);
+})
+
+test('Divide 50 / 5 to be equal to 10', () => {
+  const expectedValue = 10;
+  const result = calculator.division(50, 5);
+  expect(result).toBe(expectedValue);
+})
+
+test('Multiply 2 x 5 to be equal to 10', () => {
+  const expectedValue = 10;
+  const result = calculator.multiplication(2, 5);
+  expect(result).toBe(expectedValue);
+})
